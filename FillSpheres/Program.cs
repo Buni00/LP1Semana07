3e2f580132678412a6900c79ab668a82e943ec29
Buni00 <bruno_hrf@outlook.com>
2023-04-27 22:24:16 +0100

    Create spheres and throw them

--- FillSpheres/Program.cs
+++ FillSpheres/Program.cs
@@ -6,7 +6,16 @@ namespace FillSpheres
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Sphere sphere1 = new Sphere(new Color(123, 123, 123), 2.0f);
+            Sphere sphere2 = new Sphere(new Color(50, 50, 50), 3.0f);
+            Sphere sphere3 = new Sphere(new Color(0, 0, 0), 1.0f);
+
+            sphere1.Throw();
+            sphere2.Throw();
+            sphere3.Throw();
+            sphere2.Throw();
+            sphere3.Throw();
+            sphere2.Throw();
         }
     }
 }
