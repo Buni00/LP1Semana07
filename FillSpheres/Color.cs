d8e7b67ae0a30e9706e9507ddd6e81beac9f48c2
Buni00 <bruno_hrf@outlook.com>
2023-04-27 19:31:46 +0100

    Add Contructors to class Color

---
+++ FillSpheres/Color.cs
@@ -0,0 +1,26 @@
+namespace FillSpheres
+
+    public class Color
+    {
+        private int red;
+        private int green;
+        private int blue;
+        private int alpha;
+
+        public Color(int red, int green, int blue, int alpha)
+        {
+            this.red = red;
+            this.green = green;
+            this.blue = blue;
+            this.alpha = alpha;
+        }
+
+        public Color(int red, int green, int blue)
+        {
+            this.red = red;
+            this.green = green;
+            this.blue = blue;
+        }
+
+
+    }
