d8469c4ce0a7a4c27b11592a06f9e825d9c1ab02
Buni00 <bruno_hrf@outlook.com>
2023-04-27 22:17:32 +0100

    Add Sphere class with constructor

---
+++ FillSpheres/Sphere.cs
@@ -0,0 +1,25 @@
+namespace FillSpheres
+{
+    public class Sphere
+    {
+        private Color color;
+        private float radius;
+        private int nThrows;
+
+        public Sphere (Color color, float radius)
+        {
+            this.color = color;
+            this.radius = radius;
+            this.nThrows = 0;
+        }
+
+
+        public void Pop()
+        {
+            radius = 0;
+        }
+
+
+    }
+
+}
