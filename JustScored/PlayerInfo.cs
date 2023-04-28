d5dad1492d7b6a0d668adc68882d08524f451107
Bruno Fernandes <a22103346@alunos.ulht.pt>
2023-04-28 14:30:57 +0100

    Add properties

--- JustScored/PlayerInfo.cs
+++ JustScored/PlayerInfo.cs
@@ -2,18 +2,60 @@ namespace JustScored
 {
     public class PlayerInfo
     {
-        public float MaxScore {get; set;}
+        public string Name {get;}
+        public float maxScore;
         public int GamesPlayed {get; set;} 
         public int GamesWon {get; set;} 
 
-        public PlayerInfo ( float maxScore, int gamesPlayed. int gamesWon)
+        public PlayerInfo ( string name)
         {
-            MaxScore = maxscore;
-            GamesPlayed = gamesPlayed;
-            GamesWon = gamesWon;
+            Name = name;
+            MaxScore = 0f;
+            GamesPlayed = 0;
+            GamesWon = 0;
         } 
 
-        
+
+        public float MaxScore
+        {
+            get {return maxScore; }
+            set 
+            {
+                if (value > maxScore)
+                {
+                    maxScore = value;
+                }
+            }
+        }
+
+        public float RateOfSuccess
+        {
+            get 
+            {
+                if (GamesPlayed == 0)
+                {
+                    return 0f;
+                }
+                else
+                {
+                    float winRate = GamesWon/GamesPlayed;
+                    return winRate;
+                }
+                    
+
+            }
+        }
+
+        public void AddGame(bool win)
+        {
+            if (win == true)
+            {
+                GamesWon++;
+            }
+            
+        }
+
+
 
         
 
