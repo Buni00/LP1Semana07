82bce022cf0efc5ec6b4645180b6a85e096f225b
Bruno Fernandes <a22103346@alunos.ulht.pt>
2023-04-28 14:00:54 +0100

    Add PlayerInfo class and constructor

---
+++ JustScored/PlayerInfo.cs
@@ -0,0 +1,25 @@
+namespace JustScored
+{
+    public class PlayerInfo
+    {
+        public float MaxScore {get; set;}
+        public int GamesPlayed {get; set;} 
+        public int GamesWon {get; set;} 
+
+        public PlayerInfo ( float maxScore, int gamesPlayed. int gamesWon)
+        {
+            MaxScore = maxscore;
+            GamesPlayed = gamesPlayed;
+            GamesWon = gamesWon;
+        } 
+
+        
+
+        
+
+
+        
+
+
+    }
+}
